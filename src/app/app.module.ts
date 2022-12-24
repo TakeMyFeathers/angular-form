e57079a05fb8705902ce6d779ebc36f22c93a292
Marcin Brzozowski <brzozowski593@gmail.com>
2022-12-24 15:43:15 +0100

    feat: create user-table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AddUserComponent } from './form/add-user/add-user.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AddUserComponent} from './form/add-user/add-user.component';
 import {HttpClientModule} from "@angular/common/http";
-import { UserTableComponent } from './user-table/user-table.component';
+import {UserTableComponent} from './user-table/user-table.component';
 
 @NgModule({
   declarations: [
@@ -21,9 +21,9 @@ import { UserTableComponent } from './user-table/user-table.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule,
+    HttpClientModule,
     ReactiveFormsModule,
-    HttpClientModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-table/user-table.component.ts
+++ src/app/user-table/user-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../model/user";
+import {UserService} from "../user.service";
+import {FormArray, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-table',
@@ -6,10 +9,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-table.component.scss']
 })
 export class UserTableComponent implements OnInit {
+  form = this.fb.group({
+    users: this.fb.array([])
+  });
+  data: User[] = []
 
-  constructor() { }
+  isEditable: boolean[] = []
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private userService: UserService) {
   }
 
+  get users() {
+    return this.form.controls['users'] as FormArray;
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(res => {
+      this.data = res;
+    })
+    for (const [i, row] of this.data.entries()) {
+      const {name, email, phone} = row;
+      this.users.push(
+        this.fb.group({
+          name: [name, [Validators.required]],
+          email: [email, [Validators.required, Validators.email]],
+          phone: [phone, [
+            Validators.required,
+            Validators.minLength(9),
+            Validators.maxLength(9),
+            Validators.pattern(/^[0-9]*$/i)
+          ]]
+        })
+      )
+      this.isEditable[i] = false;
+    }
+  }
+
+  toggleEdit(rowIndex: number) {
+    this.isEditable[rowIndex] = !this.isEditable[rowIndex];
+  }
+
+  deleteUser(rowIndex: number) {
+    this.users.removeAt(rowIndex);
+    this.userService.deleteUser(this.data[rowIndex].id);
+  }
+
+  updateUser(rowIndex: number) {
+    const formData = this.form.controls.users.value[0] as Omit<User, "id" | "password">;
+    const user = {
+      id: this.data[rowIndex].id,
+      name: formData.name,
+      email: formData.email,
+      password: this.data[rowIndex].password,
+      phone: formData.phone
+    }
+    this.data[rowIndex] = user;
+    this.userService.updateUser(user);
+    this.toggleEdit(rowIndex);
+  }
 }
